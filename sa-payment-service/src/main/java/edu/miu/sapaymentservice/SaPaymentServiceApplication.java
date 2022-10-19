471ffc9e074751b9f76ed83f8dffb13b12010b74
tuan.anh.nguyen <oliver.anh.nguyen@gmail.com>
2022-10-19 15:19:57 -0500

    init project

---
+++ sa-payment-service/src/main/java/edu/miu/sapaymentservice/SaPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.sapaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ sa-payment-service/src/test/java/edu/miu/sapaymentservice/SaPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.sapaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
